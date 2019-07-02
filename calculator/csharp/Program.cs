634e081be7e290d1853338e40e7bde6487f0c003
root <g.r.almeida@live.com>
2019-07-02 20:24:07 -0300

    Calculator

---
+++ calculator/csharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csharp
+{
+
+    class Calculator {
+	public int RaiseNumberSquared(int number) {
+	    return number * number;
+	}
+
+	public int SquareSumOfTheNumbersUpToTen() {
+	    int sum = 0;
+
+	    for(int number = 1; number <= 10; number++) {
+	        sum += RaiseNumberSquared(number);
+	    }
+
+	    return sum;
+	}
+
+	public int SquareSumOfTheNumbersUpToTenWithEnumerable() {
+	    return Enumerable.Range(1, 10)
+		              .Select(RaiseNumberSquared)
+			      .Sum();
+	}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+	    Console.WriteLine(calculator.SquareSumOfTheNumbersUpToTen());
+	    Console.WriteLine(calculator.SquareSumOfTheNumbersUpToTenWithEnumerable());
+	    Console.ReadKey();
+        }
+    }
+}
---
+++ calculator/fsharp/Program.fs
@@ -0,0 +1,10 @@
+﻿let raiseNumberSquared myNumber = myNumber * myNumber
+
+let sumOfSquaresOfNumbersUpToTen =
+    [1..10]
+    |> List.map raiseNumberSquared
+    |> List.sum
+
+[<EntryPoint>]
+let main argv =
+    0
